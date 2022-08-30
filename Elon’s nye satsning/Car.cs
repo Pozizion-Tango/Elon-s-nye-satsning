9553abcd76dab63fad913434fde638f007bba953
David F.N <99806140+Pozizion-Tango@users.noreply.github.com>
2022-08-30 15:09:57 +0200

    Add project files.

---
+++ Elon’s nye satsning/Car.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Elon_s_nye_satsning
+{
+    internal class Car
+    {
+        private string _colour;
+        private double battery;
+        private int distance;
+        private string display;
+        public string Colour
+        {
+            get { return _colour; }
+            set { _colour = value; }
+        }
+        public double Battery
+        {
+            get { return battery; }
+            set { battery = value; }
+        }
+        public int Distance
+        {
+            get { return distance; }
+            set { distance = value; }
+        }
+        public string Display
+        {
+            get { return display; }
+            set { display = value; }
+        }
+
+        public void ShowDisplay()//Display is shown whenever a car is choosen, to show the status of it
+        {
+            Console.Clear();
+            Console.Write($"Battery level: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write($"{Battery}%\r\n");
+            Console.ResetColor();
+            Console.Write($"Distance Drivien: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write($"{Distance} meters\r\n");
+            Console.ResetColor();
+        }
+        public bool InvalidChoice()//a little get around, to use whenever there is a wrong choice choosen. Made in here to save a few lines of code.
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid Choice");
+            Console.ResetColor();
+            return false;
+        }
+        public void Drive()//driving mode
+        {
+            while(true)//driving mode will continue to loop until the user select to stop driving mode themselves
+            {
+
+                Console.WriteLine("Press Enter To Drive 10 meters\r\n1: Drive 100 meters\r\n2: back");
+                string choice = Console.ReadLine();
+                if (choice == "" && this.Battery > 0.4)//if the battery have any juice left, it will be allowed to "drive" 10 meters
+                {
+                    this.Distance = this.Distance + 10;
+                    this.Battery = Math.Round(this.Battery - 0.5, 2);
+                    Console.Clear();
+                    this.ShowDisplay();//the new states will be put onto the selected car, and updating the Status shown to the User
+                }
+                else if (choice == "1" && this.Battery > 4)//User can also select to drive more then 10 meters, this will drain the battery more
+                {
+                    this.Distance = this.Distance + 100;
+                    this.Battery = Math.Round(this.Battery - 5, 2);
+                    Console.Clear();
+                    this.ShowDisplay();
+                }
+                else if (choice == "2")//user selects to leave driving mode
+                    break;
+                else//stop choosing something that is not there! Why are you doing this
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid Choice");
+                    Console.ResetColor();
+                    this.ShowDisplay();
+                }
+
+            }
+        }
+        public void Charge()//yup, recharges it smartass~
+        {
+            this.Battery = 100;
+            this.Distance = 0;//and resets it too i guess, does it even matter?
+        }
+    }
+}
---
+++ Elon’s nye satsning/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elon_s_nye_satsning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { //First we have some startup settings, to define the cars and what propities they hold
+            Car mazda = new Car();
+            Car tesla = new Car();
+            mazda.Colour = "Red";
+            tesla.Colour = "Blue";
+            mazda.Battery = 100;
+            tesla.Battery = 100;
+            mazda.Distance = 0;
+            tesla.Distance = 0;
+            bool invalid = false;//invaild is used throughout the program to tell the user that their input is unusable
+
+            while (true)//The first While(true) is used to play the main program itself. 
+            {
+                Console.Clear();
+                Console.WriteLine("1: for tesla\r\n2: for mazda\r\n3: Exit");
+                string choice = Console.ReadLine();//program awaits input from the user to select one of the options proposed to them
+
+                bool backtesla = false;//These false statment bools will reset  the next while loop to default
+                bool backmazda = false;
+
+                //This first If/Else statment is used to navigate the first UI
+                if (choice == "1")//Tesla Selection
+                {
+
+                    while (!backtesla)//the user selected the Tesla, and will be shown another options menu. to either select Drive, Charge, Back
+                    {
+                        tesla.ShowDisplay();//shows the status of the Tesla
+                        if (invalid)
+                            invalid = tesla.InvalidChoice();//Whatever the user typed is invaild to the selection menu
+                        Console.WriteLine("1: for driving\r\n2: for recharge\r\n3: back");
+                        choice = Console.ReadLine();
+                        if (choice == "1")//user selected Drive and will be giving the driving UI
+                        {
+                            tesla.ShowDisplay();//display is shown
+                            tesla.Drive();//entering driving mode
+                        }
+                        else if (choice == "2")//user selected recharge, and have their car reset
+                            tesla.Charge();
+                        else if (choice == "3")//user selected back, and is there for send back to the main menu
+                            backtesla = true;
+                        else//hey! That wasn't on the menu
+                            invalid = true;
+                    }
+                }
+                else if (choice == "2")//Mazda Selection
+                {
+
+                    while (!backmazda)//the user selected the Mazda, and will be shown another options menu. to either select Drive, Charge, Back
+                    {
+                        mazda.ShowDisplay();//shows the status of the Mazda
+                        if (invalid)
+                            invalid = mazda.InvalidChoice();//Whatever the user typed is invaild to the selection menu
+                        Console.WriteLine("1: for driving\r\n2: for recharge\r\n3: back");
+                        choice = Console.ReadLine();
+                        if (choice == "1")//user selected Drive and will be giving the driving UI
+                        {
+                            mazda.ShowDisplay();//display is shown
+                            mazda.Drive();//entering driving mode
+                        }
+                        else if (choice == "2")//user selected recharge, and have their car reset
+                            mazda.Charge();
+                        else if (choice == "3")//user selected back, and is there for send back to the main menu
+                            backmazda = true;
+                        else//hey! That wasn't on the menu
+                            invalid = true;
+                    }
+                }
+                else if (choice == "3")//Exit, terminating the program
+                    break;
+                else//oooohh wupsi, whatever you typed is invalid for the UI selection
+                {
+                    Console.Clear();
+                    invalid = tesla.InvalidChoice();//that is says Tesla doesn't matter
+                    Console.ReadKey();
+                }
+                    
+            } 
+        }
+    }
+}
